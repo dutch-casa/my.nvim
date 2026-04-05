5885168a3ddea844fb7d0c2811490e02bb7f2e83
Dutch Casadaban <caz.dutch@gmail.com>
2026-04-05 00:10:16 -0500

    SMEEEEAR

---
+++ lua/plugins/smear-cursor.lua
@@ -0,0 +1,10 @@
+return {
+  "sphamba/smear-cursor.nvim",
+  opts = {
+    stiffness = 0.5,
+    trailing_stiffness = 0.35,
+    trailing_exponent = 5,
+    max_length = 35,
+    legacy_computing_symbols_support = true,
+  },
+}
