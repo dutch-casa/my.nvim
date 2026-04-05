2a772efeef0ae76b6ec9cd80c1c64b2c5d6b03d5
Dutch Casadaban <caz.dutch@gmail.com>
2026-04-05 01:00:50 -0500

    Surround, theme change.

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,8 +1,18 @@
 return {
   {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    opts = {
+      disable_background = true,
+      disable_float_background = true,
+    },
+  },
+  { "rebelot/kanagawa.nvim" },
+  { "ellisonleao/gruvbox.nvim" },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "wildcharm",
+      colorscheme = "rose-pine-moon",
     },
   },
 }
